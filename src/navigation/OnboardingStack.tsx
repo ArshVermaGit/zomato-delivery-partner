b2a0982f8c1c9f934946383c84bd364b5c40ccca
Arsh Verma <arshverma.dev@gmail.com>
2025-12-14 20:18:51 +0530

    Create OnboardingStack.tsx

---
+++ src/navigation/OnboardingStack.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingWelcomeScreen from '../screens/onboarding/OnboardingWelcomeScreen';
+import DocumentUploadScreen from '../screens/onboarding/DocumentUploadScreen';
+import VehicleDetailsScreen from '../screens/onboarding/VehicleDetailsScreen';
+import BankDetailsScreen from '../screens/onboarding/BankDetailsScreen';
+import VerificationStatusScreen from '../screens/onboarding/VerificationStatusScreen';
+
+const Stack = createStackNavigator();
+
+const OnboardingStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerTitle: '', headerBackTitleVisible: false, headerTintColor: '#1C1C1C' }}>
+            <Stack.Screen
+                name="OnboardingWelcome"
+                component={OnboardingWelcomeScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="DocumentUpload"
+                component={DocumentUploadScreen}
+                options={{ title: 'Upload Documents' }}
+            />
+            <Stack.Screen
+                name="VehicleDetails"
+                component={VehicleDetailsScreen}
+                options={{ title: 'Vehicle Details' }}
+            />
+            <Stack.Screen
+                name="BankDetails"
+                component={BankDetailsScreen}
+                options={{ title: 'Bank Details' }}
+            />
+            <Stack.Screen
+                name="VerificationStatus"
+                component={VerificationStatusScreen}
+                options={{ headerShown: false, gestureEnabled: false }}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export default OnboardingStack;
