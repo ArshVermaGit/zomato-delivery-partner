b7fcdfbef552f1fbe69440e68f6e65d17ad3741e
Arsh Verma <arshverma.dev@gmail.com>
2025-12-15 21:56:28 +0530

    Update MainStack.tsx

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/home/HomeScreen';
-import OrderDetailScreen from '../screens/orders/OrderDetailScreen';
-import OrdersListScreen from '../screens/orders/OrdersListScreen';
-import EarningsScreen from '../screens/earnings/EarningsScreen';
-import PayoutScreen from '../screens/earnings/PayoutScreen';
-import IncentivesScreen from '../screens/performance/IncentivesScreen';
-import PerformanceScreen from '../screens/performance/PerformanceScreen';
-import ProfileScreen from '../screens/profile/ProfileScreen';
-import MyDetailsScreen from '../screens/profile/subscreens/MyDetailsScreen';
-import VehicleDetailsScreen from '../screens/profile/subscreens/VehicleDetailsScreen';
-import DocumentsScreen from '../screens/profile/subscreens/DocumentsScreen';
-import SafetyCenterScreen from '../screens/safety/SafetyCenterScreen';
-import SOSScreen from '../screens/safety/SOSScreen';
-import AccidentReportScreen from '../screens/safety/AccidentReportScreen';
+import { createStackNavigator, CardStyleInterpolators, HeaderStyleInterpolators, TransitionSpecs } from '@react-navigation/stack';
+import { TabNavigator } from './TabNavigator';
+
+// ... other imports ...
 
 const Stack = createStackNavigator();
 
 const MainStack = () => {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: true }}>
-            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: true,
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+                transitionSpec: {
+                    open: TransitionSpecs.TransitionIOSSpec,
+                    close: TransitionSpecs.TransitionIOSSpec,
+                },
+                gestureEnabled: true,
+                gestureDirection: 'horizontal',
+            }}
+        >
+            <Stack.Screen name="MainTabs" component={TabNavigator} options={{ headerShown: false }} />
+
             <Stack.Screen name="OrderDetail" component={OrderDetailScreen} />
-            <Stack.Screen name="OrdersList" component={OrdersListScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Earnings" component={EarningsScreen} />
+            {/* Removed OrdersList, Earnings, Profile as they are now in Tabs */}
+
             <Stack.Screen name="PayoutScreen" component={PayoutScreen} options={{ headerTitle: 'Withdraw Money' }} />
             <Stack.Screen name="Incentives" component={IncentivesScreen} />
             <Stack.Screen name="Performance" component={PerformanceScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+
             <Stack.Screen name="MyDetails" component={MyDetailsScreen} options={{ headerTitle: 'My Details' }} />
             <Stack.Screen name="VehicleDetails" component={VehicleDetailsScreen} options={{ headerTitle: 'Vehicle Details' }} />
             <Stack.Screen name="Documents" component={DocumentsScreen} options={{ headerTitle: 'My Documents' }} />
