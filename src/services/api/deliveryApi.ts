bf86b5581e8d37d8b5a513b49598cdcc5c65b939
Arsh Verma <arshverma.dev@gmail.com>
2025-12-18 20:18:51 +0530

    Create deliveryApi.ts

---
+++ src/services/api/deliveryApi.ts
@@ -0,0 +1,37 @@
+import { api } from './baseApi';
+import { DeliveryPartnerStats, ActiveOrder, ApiResponse } from './api.types';
+
+export const deliveryApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        getHomeStats: builder.query<DeliveryPartnerStats, void>({
+            query: () => ({
+                url: '/delivery/stats',
+                method: 'GET',
+            }),
+            transformResponse: (response: ApiResponse<DeliveryPartnerStats>) => response.data,
+            providesTags: ['Stats'],
+        }),
+        getActiveOrder: builder.query<ActiveOrder | null, void>({
+            query: () => ({
+                url: '/delivery/active-order',
+                method: 'GET',
+            }),
+            transformResponse: (response: ApiResponse<ActiveOrder | null>) => response.data,
+            providesTags: ['Orders'],
+        }),
+        toggleAvailability: builder.mutation<void, boolean>({
+            query: (isOnline) => ({
+                url: '/delivery/availability',
+                method: 'POST',
+                data: { isOnline },
+            }),
+            invalidatesTags: ['Stats'],
+        }),
+    }),
+});
+
+export const {
+    useGetHomeStatsQuery,
+    useGetActiveOrderQuery,
+    useToggleAvailabilityMutation,
+} = deliveryApi;
